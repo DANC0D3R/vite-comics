06145bdfb945513019a1e10762ef9564247184be
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-03 17:49:42 +0100

    Mian completato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,8 +5,24 @@ export default {
 </script>
 
 <template>
-
+    <div>
+        <span>Content goes here</span>
+    </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        height: calc(100vh - 150px - 600px);
+        
+        background-color: #191919;
+        span{
+            font-size: 30px;
+            
+            color: #FFFFFF;
+        }
+    }
 </style>
